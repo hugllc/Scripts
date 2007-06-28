0dffe08b2040b45f936cbd36f3811e748a513db1
prices <prices@b157844d-9f31-0410-aa6b-d326cbe640ab>
2007-06-28 21:44:42 +0000

    It now requires a different adodb.
    git-svn-id: https://svn.hugllc.com/0039/trunk/26@55 b157844d-9f31-0410-aa6b-d326cbe640ab

--- head.inc.php
+++ head.inc.php
@@ -36,7 +36,7 @@ define('HUGNET_FS_DIR', dirname(__FILE__));
 
 	require_once(HUGNET_FS_DIR.'/config/config.inc.php');
 //	require_once('lib/MDB_QueryWrapper.inc.php');
-	require_once('lib/adodb/adodb.inc.php');
+	require_once('adodb/adodb.inc.php');
 
 	require_once('lib/functions.inc.php');
 	require_once('lib/plugins.inc.php');
