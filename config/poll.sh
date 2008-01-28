a830d830ae7ccf85bc232be7788ac17612077a62
prices <prices@b157844d-9f31-0410-aa6b-d326cbe640ab>
2008-01-28 22:01:11 +0000

    Modified
    git-svn-id: https://svn.hugllc.com/HUGnet/trunk/Scripts@807 b157844d-9f31-0410-aa6b-d326cbe640ab

---
+++ config/poll.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo "Starting Poll..."
+/usr/bin/php-cli /home/hugnet/Scripts/endpoints/poll.php
+echo "Poll Died..."
+sleep 5
---
+++ config/updatedb.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "Starting updatedb"
+/usr/bin/php-cli /home/hugnet/Scripts/endpoints/poll.php
+echo "Updatedb died"
