a3b894ffba0d0cd5078a0b5bc3b8736e40e35ad1
Scott Price <prices@hugllc.com>
2012-03-13 11:13:07 -0500

    Work on bug #4959.

--- misc/loadprog.php
+++ misc/loadprog.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 /**
  * Loads a program into a controller board
@@ -35,26 +36,85 @@
  * @link       https://dev.hugllc.com/index.php/Project:Scripts
  *
  */
-
-/** HUGnet code */
-require_once dirname(__FILE__).'/../head.inc.php';
-/** Packet log include stuff */
-require_once HUGNET_INCLUDE_PATH.'/containers/PacketContainer.php';
-require_once HUGNET_INCLUDE_PATH.'/containers/DeviceContainer.php';
+require_once dirname(__FILE__).'/../../HUGnetLib/src/cli/Daemon.php';
+require_once dirname(__FILE__).'/../../HUGnetLib/src/cli/Args.php';
+require_once dirname(__FILE__).'/../../HUGnetLib/src/containers/DeviceContainer.php';
 
 print "loadprog.php\n";
 print "Starting...\n";
 
-print "Using GatewayKey ".$GatewayKey.", DeviceKey ".$DeviceKey."\n";
+$config = &\HUGnet\cli\Args::factory(
+    $argv, $argc,
+    array(
+        "i" => array("name" => "DeviceID", "type" => "string", "args" => true),
+        "D" => array("name" => "Data", "type" => "string", "args" => true),
+    )
+);
+$conf = $config->config();
+$conf["network"]["channels"] = 1;
+$cli = &\HUGnet\cli\Daemon::factory($conf);
 
-$config = &ConfigContainer::singleton("/etc/hugnet/config.inc.php");
-$config->verbose($hugnet_config["verbose"] + HUGnetClass::VPRINT_NORMAL);
-$dev = new DeviceContainer();
-$dev->getRow(hexdec($DeviceID));
-$dev->runBootloader();
-$dev->readConfig();
-var_dump($dev->writeProgram($pktData));
-$dev->readConfig();
+$path = dirname($config->D);
+if (empty($path)) {
+    $path = ".";
+}
+$file = basename($config->D);
+if (empty($file)) {
+    die("Filename must be provided!\n");
+}
+
+$DevID = hexdec($config->i);
+print "Reprogramming device ".sprintf("%06X", $DevID)."\n";
+print "Getting the configuration...  ";
+$oldConfig = $cli->system()->device($DevID)->network()->config();
+if (!is_object($oldConfig) || is_null($oldConfig->Reply())) {
+    die("Failure\n");
+}
+print "Success!\n";
+print "Running the bootloader...  ";
+if (!$cli->system()->device($DevID)->network()->runBootloader()) {
+    die("Failure\n");
+}
+print "Success!\n";
+print "Getting the bootloader configuration...  ";
+$bootConfig = $cli->system()->device($DevID)->network()->config();
+if (!is_object($bootConfig) || is_null($bootConfig->Reply())) {
+    die("Failure\n");
+}
+print "Success!\n";
+
+
+$firmware = new FirmwareTable();
+$firmware->fromFile($file, $path);
+print "Found firmware ".$firmware->FWPartNum." v".$firmware->Version."\n";
+
+print "Writing the code...\n";
+$code = $cli->system()->device($DevID)->network()->writeFlashBuffer(
+    $firmware->getCode()
+);
+if (!$code) {
+    die("Failed to write code\n");
+}
+print "Writing the data...\n";
+$data = $cli->system()->device($DevID)->network()->writeE2Buffer(
+    $firmware->getData(), 10
+);
+if (!$data) {
+    die("Failed to write data\n");
+}
+
+print "Setting the CRC...  ";
+$crc = $cli->system()->device($DevID)->network()->setCRC();
+if ($crc === false) {
+    die("Failure\n");
+}
+print $crc."\n";
+
+print "Running the application...  ";
+if (!$cli->system()->device($DevID)->network()->runApplication()) {
+    die("Failure\n");
+}
+print "Success!\n";
 
 print "Finished\n";
 ?>
