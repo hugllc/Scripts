bca0ae4cffb88a605c8af49ffad711d8dcf5ae6a
Scott Price <prices@hugllc.com>
2012-11-15 10:12:47 -0600

    Fixed bug #7025.  I split this up into 4 different deb packages, depending on what the user wants.

--- deb/home/hugnet/device.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd /home/hugnet/Scripts/endpoints
-/usr/bin/php device.php
-sleep 20
--- deb/home/hugnet/endpoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd /home/hugnet/Scripts/endpoints
-/usr/bin/php endpoint.php
-sleep 20
