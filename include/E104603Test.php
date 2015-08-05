4f815d531e1428607c7f76225d71af5a4b6ba3d8
Jeff Liesmaki <jeffl@hugllc.com>
2015-08-05 10:43:10 -0500

    add code for test framework for battery socializer 104603 bug #8474

---
+++ include/E104603Test.php
@@ -0,0 +1,319 @@
+<?php
+/**
+ * This file houses the socket class
+ *
+ * PHP Version 5
+ * <pre>
+ * HUGnetLib is a library of HUGnet code
+ * Copyright (C) 2012 Hunt Utilities Group, LLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ * </pre>
+ *
+ * @category   Libraries
+ * @package    HUGnetLib
+ * @subpackage UI
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2015 Hunt Utilities Group, LLC
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link       http://dev.hugllc.com/index.php/Project:HUGnetLib
+ */
+
+
+/** This is the HUGnet namespace */
+namespace HUGnet\processes;
+
+/** This is our base class */
+require_once "HUGnetLib/ui/Daemon.php";
+/** This is our units class */
+require_once "HUGnetLib/devices/inputTable/Driver.php";
+/** Displays class */
+require_once "HUGnetLib/ui/Displays.php";
+
+/**
+ * This code tests, serializes and programs battery socializer endpoints 
+ *       with bootloader and application firmware.
+ *
+ * This is an endpoint test class, essentially.  It loads an endpoint without
+ * test firmware, runs the tests, writes the serial number and hardware version
+ * and then programs the bootloader firmware into the endpoint.
+ *
+ * @category   Libraries
+ * @package    HUGnetLib
+ * @subpackage UI
+ * @author     Scott Price <prices@hugllc.com>
+ * @author     Jeff Liesmaki <jeffl@hugllc.com>
+ * @copyright  2015 Hunt Utilities Group, LLC
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version    Release: 0.9.7
+ * @link       http://dev.hugllc.com/index.php/Project:HUGnetLib
+ */
+class E104603Test extends \HUGnet\ui\Daemon
+{
+
+    const TEST_ID = 0x20;
+    const EVAL_BOARD_ID = 0x30;
+
+    const HEADER_STR    = "Battery Socializer Test & Program Tool";
+
+    private $_fixtureTest;
+    private $_system;
+    private $_device;
+    private $_evalDevice;
+    private $_eptestMainMenu = array(
+                                0 => "Test 104603 Endpoint",
+                                1 => "Troubleshoot 104603 ",
+                                );
+
+    public $display;
+    /*
+    * Sets our configuration
+    *
+    * @param mixed &$config The configuration to use
+    */
+    protected function __construct(&$config)
+    {
+        parent::__construct($config);
+
+        $sys = $this->system();
+        $this->_system = &$sys;
+        $this->_evalDevice = $this->_system->device();
+        $this->_evalDevice->set("id", self:: EVAL_BOARD_ID);
+        $this->_evalDevice->set("Role","TesterEvalBoard");
+
+        $this->display = \HUGnet\ui\Displays::factory($config);
+
+    }
+
+    /**
+    * Creates the object
+    *
+    * @param array &$config The configuration to use
+    *
+    * @return null
+    */
+    static public function &factory(&$config = array())
+    {
+        $obj = new E104603Test($config);
+        return $obj;
+    }
+
+    /**
+    ************************************************************
+    *
+    *                          M A I N 
+    *
+    ************************************************************
+    *
+    * It would be nice to have a test fixture ID test to verify
+    * that the fixture matches the menu selection.
+    * 
+    * @return null
+    */
+    public function main()
+    {
+        $exitTest = false;
+        $result;
+
+        do{
+            $this->display->clearScreen();
+            $selection = $this->display->displayMenu(self::HEADER_STR, 
+                            $this->_eptestMainMenu);
+
+            if (($selection == "A") || ($selection == "a")) {
+                $this->_test104603Main();
+            } else if (($selection == "B") || ($selection == "b")){
+                $this->_troubleshoot104603Main();
+            } else {
+                $exitTest = true;
+                $this->out("Exit 104603 Tool");
+            }
+
+        } while ($exitTest == false);
+    }
+
+
+
+
+    /*****************************************************************************/
+    /*                                                                           */
+    /*                     T E S T   R O U T I N E S                             */
+    /*                                                                           */
+    /*****************************************************************************/
+
+ 
+    /**
+    ************************************************************
+    * Main Test Routine
+    * 
+    * This is the main routine for testing, serializing and 
+    * programming in the bootloader for HUGnet endpoints.
+    *
+    * @return void
+    *   
+    */
+    private function _test104603Main()
+    {
+        $exitTest = false;
+
+        $result = $this->_checkEvalBoard();
+        $this->out("Not Done!");
+        $choice = readline("\n\rHit Enter to Continue: ");
+    }
+
+   
+    /**
+    ************************************************************
+    * Main Clone Routine
+    *
+    * This is the main routine for testing, serializing and 
+    * programming the 003912 endpoint.  
+    * 
+    * @return void
+    *
+    */
+    private function _troubleshoot104603Main()
+    {
+
+        $this->out("Not Done!");
+        $choice = readline("\n\rHit Enter to Continue: ");
+    }
+
+     /**
+    ************************************************************
+    * Check Eval Board Routine
+    *
+    * This function pings the Xmega-E5 Eval board
+    * and returns the results.
+    *
+    * @return boolean $testResult   
+    */
+    private function _checkEvalBoard()
+    {
+        
+        
+
+        $Result = $this->_pingEndpoint(self::EVAL_BOARD_ID);
+        if ($Result = true) {
+            $this->_system->out("Eval Board Responding!");
+        } else {
+            $this->_system->out("Eval Board Failed to Respond!");
+        }
+
+
+        return $Result;
+
+    }
+
+
+    /*****************************************************************************/
+    /*                                                                           */
+    /*                    H U G N ET   R O U T I N E S                           */
+    /*                                                                           */
+    /*****************************************************************************/
+
+
+
+    /**
+    ***********************************************************
+    * Send a Ping routine
+    *
+    * This function pings the endpoint
+    * with the test serial number and 
+    * checks to see that the endpoint
+    * responds.
+    *
+    * @param int $Sn Endpoint Serial Numberd
+
+    *
+    * @return true, if endpoint responds
+    *               otherwise false
+    *
+    */
+
+    private function _pingEndpoint($Sn)
+    {
+        $dev = $this->_system->device($Sn);
+        $result = $dev->action()->ping();
+        //var_dump($result);
+        return true;
+    }
+
+
+    /**
+    **********************************************************
+    * Send a Packet routine
+    *
+    * This function will send a packet
+    * to an endpoint with a command 
+    * and data.
+    *
+    * @param int    $Sn      endpoint serial
+    * @param int    $Cmd     hex command number
+    * @param string $DataVal ascii hex data string  
+    *
+    * @return reply data from endpoint
+    *
+    */
+    private function _sendPacket($Sn, $Cmd, $DataVal)
+    {
+
+        $this->_system->out("Data is ".$DataVal);
+
+
+        $dev = $this->_system->device($Sn);
+        $pkt = $dev->action()->send(
+            array(
+                "Command" => $Cmd,
+                "Data" => $DataVal,
+            ),
+            null,
+            array(
+                "timeout" => $dev->get("packetTimeout")
+            )
+        );
+
+        if (is_object($pkt)) {
+            $this->_system->out(
+                "From: ".$pkt->From()
+                ." -> To: ".$pkt->To()
+                ."  Command: ".$pkt->Command()
+                ."  Type: ".$pkt->Type(),
+                2
+            );
+           
+            $data = $pkt->Data();
+            if (!empty($data)) {
+                $this->_system->out("Data: ".$data, 2);
+            }
+
+            $data = $pkt->Reply();
+            if (is_null($data)) {
+                $this->_system->out("No Reply", 2);
+            } else if (!empty($data)) {
+                $this->_system->out("Reply Data: ".$data, 2);
+            } else {
+                $this->_system->out("Empty Reply", 2);
+            }
+        }
+
+        return $data;
+    }
+
+
+
+}
+?>
