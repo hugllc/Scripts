90a67106ed40f4076b43ecb0ab8bebeac2a3bbbe
Jeff Liesmaki <jeffl@hugllc.com>
2015-08-05 16:20:20 -0500

    added skeleton test code to 104603 script bug #8474

--- include/E104603Test.php
+++ include/E104603Test.php
@@ -67,6 +67,8 @@ class E104603Test extends \HUGnet\ui\Daemon
     const TEST_ID = 0x20;
     const EVAL_BOARD_ID = 0x30;
 
+    const TEST_ANALOG_COMMAND  = 0x20;
+
     const HEADER_STR    = "Battery Socializer Test & Program Tool";
 
     private $_fixtureTest;
@@ -161,6 +163,28 @@ class E104603Test extends \HUGnet\ui\Daemon
     * 
     * This is the main routine for testing, serializing and 
     * programming in the bootloader for HUGnet endpoints.
+    * 
+    * Test Steps 
+    *   1: Check Eval Board
+    *   2: Power Up DUT test
+    *   3: Load test firmware into DUT
+    *   4: Ping DUT
+    *   5: DUT to turn on switched 3V
+    *   6: DUT to return Bus Voltage
+    *   7: Tester measures Bus & 3V_SW
+    *   8: DUT to return board thermistor values
+    *   9: DUT to cycle through LED's
+    *  10: Run Port 1 Load Test
+    *  11: Run Port 2 Load Test
+    *  12: Run Port 1 Fault Test
+    *  13: Run Port 2 Fault Test
+    *  14: Run External Therm 1 Test
+    *  15: Run External Therm 2 Test
+    *  16: Load DUT bootloader and config data
+    *  17: Load DUT application code
+    *  18: Power cycle DUT & verify communications
+    *  19: Power Down
+    *  20: Display passed and log data
     *
     * @return void
     *   
@@ -168,11 +192,46 @@ class E104603Test extends \HUGnet\ui\Daemon
     private function _test104603Main()
     {
         $exitTest = false;
+        $this->display->clearScreen();
 
         $result = $this->_checkEvalBoard();
-        $this->out("Not Done!");
+
+        $dataStr = $this->_readADCinput(4);
+
+        $this->out("Bus Voltage = ".$dataStr." volts");
+
         $choice = readline("\n\rHit Enter to Continue: ");
     }
+
+
+    /**
+    ************************************************************
+    * Read Eval Board ADC Input Routine
+    *
+    * This function reads the Eval board analog input
+    * specified by the input parameter and returns the reply 
+    * data.
+    * 
+    * @return string $reply data
+    *
+    */
+    private function _readADCinput($inputNum)
+    {
+        $idNum = self::EVAL_BOARD_ID;
+        $cmdNum = self::TEST_ANALOG_COMMAND;
+        $dataVal = sprintf("0%s",$inputNum);
+        
+        $ReplyData = $this->_sendPacket($idNum, $cmdNum, $dataVal);
+
+        $newData = $this->_convertReplyData($ReplyData);
+
+        $steps = 1.0/ pow(2,11);
+        $volts = $steps * $newData;
+        $volts = $volts * 21;
+
+        return $volts;
+    }
+
 
    
     /**
@@ -217,6 +276,127 @@ class E104603Test extends \HUGnet\ui\Daemon
         return $Result;
 
     }
+
+
+    /**
+    ***********************************************************
+    * Power DUT Routine
+    * 
+    * This function powers up the 10460301 board, measures the 
+    * bus voltage and the 3.3V to verify operation for the next
+    * step which is loading the test firmware.
+    *
+    * @return boolean result
+    */
+    private function _powerDUTtest()
+    {
+        /* not done */
+        /* steps in Power DUT test */
+        /* 1: Send command to functional tester */
+        /* 2: Test Results */
+
+
+    }
+
+
+    /**
+    ************************************************************
+    * Load test firmware routine
+    *
+    * This function loads the 10460301 endpoint with the test 
+    * firmware.  It programs the device with a test serial 
+    * number which will eventually be replaced after testing within
+    * the original board serial number entered by the user.  The 
+    * firmware is load through the AVR2 serial programmer.
+    *
+    * @return int $result  
+    */
+    private function _loadTestFirmware()
+    {
+        $output = array();
+
+        $this->display->displayHeader("Loading Test Firmware");
+
+
+        $Prog = "make -C ~/code/HOS 104607test-install SN=0x0000000020";
+        exec($Prog, $output, $return);
+
+        if ($return == 0) {
+            $result = true;
+        } else {
+            $result = false;
+        }
+
+        return $result;
+
+    }
+
+    /**
+    ************************************************************
+    * Test Socializer Routine
+    *
+    * This function runs the tests in the endpoint test firmware
+    * and returns the results.
+    *
+    * @return boolean $testResult
+    *
+    */
+    private function _testEndpoint()
+    {
+        $Result = true;    
+   
+        /*****************************/
+        /* Steps in endpoint test    */
+        /* 1: 3.3V Switched Test     */
+        /* 2: Board Thermistors Test */
+        /* 3: LED Test               */
+        /* 4: Port 1 Load Test       */
+        /* 5: Port 1 default Test    */
+        /* 6: Port 2 Load Test       */
+        /* 7: Port 2 default Test    */
+        /* 8: Vbus Load Test         */
+        /* 9: Ext Thermistor 1 Test  */
+        /* 10: Ext Thermistor 2 Test */
+        /*****************************/
+
+        return $Result;
+    }
+
+    
+
+
+    /*****************************************************************************/
+    /*                                                                           */
+    /*              D A T A   C O N V E R S I O N   R O U T I N E S              */
+    /*                                                                           */
+    /*****************************************************************************/
+
+
+    /**
+    ************************************************************
+    * Convert Reply Data String
+    *
+    * This function changes the bytes in the input string
+    * from little endian to big endian so the hex string
+    * can be converted to an integer.
+    *
+    * @return int $result of conversion
+    *
+    */
+    private function _convertReplyData(&$inString)
+    {
+        $size = strlen($inString);
+        $newString = "00";
+        $newString = $newString.substr($inString, 4, 2);
+        $newString = $newString.substr($inString, 2, 2);
+        $newString = $newString.substr($inString, 0, 2);
+        $newString = "0x".$newString;
+        $newVal = 0 + $newString;
+        
+        return $newVal;
+
+    }
+
 
 
     /*****************************************************************************/
